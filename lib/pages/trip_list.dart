169b620f43f5c680ebeb8f0c77f65216079c4fc7
Rajan Phadnis <22627030+rajanphadnis@users.noreply.github.com>
2023-10-15 02:34:04 -0400

    added trip card

--- lib/pages/trip_list.dart
+++ lib/pages/trip_list.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:travel_planner/widgets/trip_card.dart';
 
 class TripList extends StatelessWidget {
   const TripList({super.key});
@@ -10,20 +11,11 @@ class TripList extends StatelessWidget {
       options: CarouselOptions(
         height: MediaQuery.of(context).size.height * 0.75,
         enlargeCenterPage: true,
-        // aspectRatio: isMobile(context) ? 9 / 16 : 16 / 9,
       ),
       items: [1, 2, 3, 4, 5].map((i) {
         return Builder(
           builder: (BuildContext context) {
-            return Container(
-              width: MediaQuery.of(context).size.width,
-              margin: const EdgeInsets.symmetric(horizontal: 5.0),
-              decoration: const BoxDecoration(color: Colors.amber),
-              child: Text(
-                'text $i',
-                style: const TextStyle(fontSize: 16.0),
-              ),
-            );
+            return TripCard(i);
           },
         );
       }).toList(),
--- lib/widgets/trip_card.dart
+++ lib/widgets/trip_card.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
 class TripCard extends StatelessWidget {
-  const TripCard({super.key});
+  final int index;
+  const TripCard(this.index, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 41, 41, 41),
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(20),
+          ),
+          onTap: () {
+            debugPrint("test");
+          },
+          child: Center(
+            child: Text(
+              'Trip #$index',
+              style: const TextStyle(fontSize: 30.0),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
